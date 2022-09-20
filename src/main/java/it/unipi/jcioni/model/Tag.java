a579d1ed6d66f0ca33c3f910add0edb941bc7bd8
JacopoCioni <cionijacopo4@gmail.com>
2022-09-20 19:30:15 +0200

    Add Tag

---
+++ src/main/java/it/unipi/jcioni/model/Tag.java
@@ -0,0 +1,13 @@
+package it.unipi.jcioni.model;
+
+public class Tag {
+    private final String value;
+
+    public Tag(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
