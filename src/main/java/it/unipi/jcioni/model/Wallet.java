dc85c2c30abb265776ed79ef9c2e3978e8ef9a99
JacopoCioni <cionijacopo4@gmail.com>
2022-09-21 09:58:15 +0200

    Modified, added a ThreadSafe collection for wallet transaction

--- src/main/java/it/unipi/jcioni/model/Wallet.java
+++ src/main/java/it/unipi/jcioni/model/Wallet.java
@@ -1,9 +1,13 @@
 package it.unipi.jcioni.model;
 
+import java.util.concurrent.ConcurrentLinkedDeque;
+
 public class Wallet {
 
     private final User userWallet;
     private int balance;
+    // Coda ThreadSafe, mi immagino che ci siano più Thread in accesso a questa collezione
+    private ConcurrentLinkedDeque<Transaction> transactions;
 
     public Wallet(User userWallet) {
         this.userWallet = userWallet;
