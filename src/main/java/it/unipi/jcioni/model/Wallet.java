cfe91a01281f426caa308dc22bdd1e0a8e214029
JacopoCioni <cionijacopo4@gmail.com>
2022-09-21 11:58:55 +0200

    Add comments, fixed errors, modified getter & setter

--- src/main/java/it/unipi/jcioni/model/Wallet.java
+++ src/main/java/it/unipi/jcioni/model/Wallet.java
@@ -5,23 +5,39 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 public class Wallet {
 
     private final User userWallet;
-    private int balance;
-    // Coda ThreadSafe, mi immagino che ci siano più Thread in accesso a questa collezione
+
+    // ThreadSafe queue, you might have multiple threads wanting to access this collection
     private ConcurrentLinkedDeque<Transaction> transactions;
 
     public Wallet(User userWallet) {
         this.userWallet = userWallet;
+        transactions = new ConcurrentLinkedDeque<>();
     }
 
     public User getUserWallet() {
         return userWallet;
     }
 
-    public int getBalance() {
+    public ConcurrentLinkedDeque<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public double getBalance() {
+        double balance = 0;
+        for (Transaction t : transactions) {
+            balance = balance + t.getValue();
+        }
         return balance;
     }
 
-    public void setBalance(int balance) {
-        this.balance = balance;
+    /*
+    This method allows you to add a new transaction in your wallet.
+    @param value of the new transaction
+    @param message of the new transaction
+    @return new balance
+     */
+    public double addTransaction(double value, String msg) {
+        transactions.add(new Transaction(value, msg));
+        return getBalance();
     }
 }
