98f277543ffcbf80a9e52053bc4617b6fd8cf553
JacopoCioni <cionijacopo4@gmail.com>
2022-09-21 11:21:40 +0200

    Modified Transaction, added constructor & getter

--- src/main/java/it/unipi/jcioni/model/Transaction.java
+++ src/main/java/it/unipi/jcioni/model/Transaction.java
@@ -1,5 +1,29 @@
 package it.unipi.jcioni.model;
 
+/*
+    This class represents the single winsome transaction
+ */
 public class Transaction {
 
+        private final double value;
+        private final String msg;
+        private final long date;
+
+    public Transaction(double value, String msg, long date) {
+        this.value = value;
+        this.msg = msg;
+        this.date = System.currentTimeMillis();
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public long getDate() {
+        return date;
+    }
 }
