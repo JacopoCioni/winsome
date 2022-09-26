e3a92746d671a2ba2f77ea88da1ac980bf1b65dd
JacopoCioni <cionijacopo4@gmail.com>
2022-09-26 19:28:56 +0200

    Added new exceptions, fixed exception errors

---
+++ core/src/main/java/it/unipi/jcioni/winsome/core/exception/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package it.unipi.jcioni.winsome.core.exception;
+
+public class InvalidOperationException extends Exception {
+    public InvalidOperationException() {
+        super("Invalid operation");
+    }
+}
--- core/src/main/java/it/unipi/jcioni/winsome/core/exception/SameUserException.java
+++ core/src/main/java/it/unipi/jcioni/winsome/core/exception/SameUserException.java
@@ -1,6 +1,6 @@
 package it.unipi.jcioni.winsome.core.exception;
 
-public class SameUserException extends Exception{
+public class SameUserException extends Exception {
     public SameUserException() {
         super("This is the same user");
     }
