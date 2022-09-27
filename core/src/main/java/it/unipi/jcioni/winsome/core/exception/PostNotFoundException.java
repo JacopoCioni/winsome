f3d657abbdb21b6ac30e728ffa0a24efa46803ee
JacopoCioni <cionijacopo4@gmail.com>
2022-09-27 20:35:14 +0200

    added PostNotFoundException

---
+++ core/src/main/java/it/unipi/jcioni/winsome/core/exception/PostNotFoundException.java
@@ -0,0 +1,7 @@
+package it.unipi.jcioni.winsome.core.exception;
+
+public class PostNotFoundException extends Exception {
+    public PostNotFoundException() {
+        super("Post not found");
+    }
+}
