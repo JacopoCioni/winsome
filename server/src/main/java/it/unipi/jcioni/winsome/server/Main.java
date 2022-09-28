ea41b4c2eebe9d88bb652d8cb66f5f577202e86e
JacopoCioni <cionijacopo4@gmail.com>
2022-09-28 21:25:15 +0200

    Add server

--- server/src/main/java/it/unipi/jcioni/winsome/server/Main.java
+++ server/src/main/java/it/unipi/jcioni/winsome/server/Main.java
@@ -1,7 +1,32 @@
 package it.unipi.jcioni.winsome.server;
 
+import com.sun.jdi.IntegerValue;
+import it.unipi.jcioni.winsome.core.service.WinsomeService;
+import it.unipi.jcioni.winsome.core.service.impl.WinsomeServiceImpl;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            /* Creazione di un'istanza dell'oggetto EUStatsService */
+            WinsomeServiceImpl statsService = new WinsomeServiceImpl();
+            /* Esportazione dell'Oggetto */
+            WinsomeService stub = (WinsomeService) UnicastRemoteObject.exportObject(statsService, 0);
+            /* Creazione di un registry sulla porta args[0] */
+            int port = args.length > 0 && args[0] != null && args[0].length() > 0 ? Integer.parseInt(args[0]) : 8080;
+            LocateRegistry.createRegistry(port);
+            /*Pubblicazione dello stub nel registry */
+            Registry r = LocateRegistry.getRegistry(port);
+            r.rebind("WINSOME-SERVER", stub);
+            System.out.println("Server ready");
+        }
+        /* If any communication failures occur... */
+        catch (RemoteException e) {
+            System.out.println("Communication error " + e.toString());
+        }
     }
 }
--- src/main/java/it/unipi/jcioni/Main.java
+++
@@ -1,7 +0,0 @@
-package it.unipi.jcioni;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
