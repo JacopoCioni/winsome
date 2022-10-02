16ff6dac56bf4585e254f59afd08e522380e29f8
JacopoCioni <cionijacopo4@gmail.com>
2022-10-02 23:13:45 +0200

    Added client & server main, they have to be fixed

--- client/src/main/java/it/unipi/jcioni/winsome/client/Main.java
+++ client/src/main/java/it/unipi/jcioni/winsome/client/Main.java
@@ -1,7 +1,91 @@
 package it.unipi.jcioni.winsome.client;
 
+import it.unipi.jcioni.winsome.core.service.WinsomeService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner read = new Scanner(System.in);
+        Socket socket;
+        while (true) {
+            try {
+                socket = new Socket(WinsomeService.SERVER_ADDRESS, WinsomeService.SERVER_TCP_PORT);
+            } catch (IOException e) {
+                System.err.println("Impossibile connettersi al server, riprovare più tardi.");
+                break;
+            }
+
+            System.out.println("Connessione TCP stabilita.");
+
+            Registry reg;
+            WinsomeService stub;
+            try {
+                reg = LocateRegistry.getRegistry(WinsomeService.SERVER_ADDRESS, WinsomeService.SERVER_RMI_PORT);
+                stub = (WinsomeService) reg.lookup(WinsomeService.RMI_SERVER_REGISTRY_NAME);
+            } catch (AccessException e) {
+                System.err.println("Il registro non è raggiungibile.");
+                e.printStackTrace();
+                return;
+            } catch (NotBoundException | RemoteException e) {
+                System.err.println("Errore RMI");
+                e.printStackTrace();
+                return;
+            }
+
+            try {
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String command;
+                while (true) {
+                    try {
+                        command = read.nextLine();
+                    } catch (NoSuchElementException e) {
+                        continue;
+                    }
+                    command = command.trim();
+                    String[] arguments = command.split("\\s+");
+                    command = arguments[0];
+                    arguments = (String[]) Arrays.stream(arguments).collect(Collectors.toList()).subList(1, arguments.length).toArray();
+                    switch (command) {
+                        case "register":
+                            try {
+                                if (arguments.length < 3) throw new ArrayIndexOutOfBoundsException();
+                                String username = arguments[0];
+                                String password = arguments[1];
+                                String tags = "";
+                                for (int i = 2; i < arguments.length; i++) {
+                                    tags += " " + arguments[i];
+                                }
+                                tags = tags.trim();
+                                boolean response = stub.register(username, password, tags);
+                                if (response) {
+                                    System.out.println("Registrazione dell'utente " + username + " effettuata con successo.");
+                                } else {
+                                    System.out.println("Registrazione dell'utente " + username + " fallita.");
+                                }
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                System.err.println("Richiesti almeno 3 argomenti, massimo 7.\n<username> <password> [tags]");
+                            } catch (RemoteException ignored) { }
+                            break;
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- server/src/main/java/it/unipi/jcioni/winsome/server/Main.java
+++ server/src/main/java/it/unipi/jcioni/winsome/server/Main.java
@@ -4,29 +4,91 @@ import com.sun.jdi.IntegerValue;
 import it.unipi.jcioni.winsome.core.service.WinsomeService;
 import it.unipi.jcioni.winsome.core.service.impl.WinsomeServiceImpl;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
+
+        int serverPort = args.length > 0 && args[0] != null && args[0].length() > 0 ? Integer.parseInt(args[0]) : 8080;
+        int rmiPort = args.length > 0 && args[1] != null && args[1].length() > 0 ? Integer.parseInt(args[1]) : 6969;
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        ConcurrentLinkedDeque<Socket> sockets = new ConcurrentLinkedDeque<>();
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
         try {
-            /* Creazione di un'istanza dell'oggetto EUStatsService */
-            WinsomeServiceImpl statsService = new WinsomeServiceImpl();
+            /* Creazione di un'istanza dell'oggetto WinsomeServiceImpl */
+            WinsomeServiceImpl winsomeServ = new WinsomeServiceImpl();
             /* Esportazione dell'Oggetto */
-            WinsomeService stub = (WinsomeService) UnicastRemoteObject.exportObject(statsService, 0);
-            /* Creazione di un registry sulla porta args[0] */
-            int port = args.length > 0 && args[0] != null && args[0].length() > 0 ? Integer.parseInt(args[0]) : 8080;
-            LocateRegistry.createRegistry(port);
+            WinsomeService stub = (WinsomeService) UnicastRemoteObject.exportObject(winsomeServ, 0);
+            /* Creazione di un registry sulla porta rmiPort */
+            LocateRegistry.createRegistry(rmiPort);
             /*Pubblicazione dello stub nel registry */
-            Registry r = LocateRegistry.getRegistry(port);
+            Registry r = LocateRegistry.getRegistry(rmiPort);
             r.rebind("WINSOME-SERVER", stub);
-            System.out.println("Server ready");
+            System.out.println("RMI pronto sulla porta " + rmiPort);
         }
         /* If any communication failures occur... */
         catch (RemoteException e) {
-            System.out.println("Communication error " + e.toString());
+            System.out.println("Communication error " + e.getMessage());
+            e.printStackTrace();
+            return;
         }
+        try {
+            serverSocket = new ServerSocket(serverPort);
+            System.out.println("Server pronto sulla porta " + serverPort);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while(true) {
+            try {
+                //bloccante fino a quando non avviene una connessione
+                clientSocket = serverSocket.accept();
+                sockets.add(clientSocket);
+                executor.submit(new Handler(clientSocket));
+            } catch (SocketException e) {
+                e.printStackTrace();
+                break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        //Chiusura socket dei client
+        for (Socket s: sockets) {
+            try {
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //Chiusura socket server
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Chiusura del pool
+        executor.shutdown();
+        try {
+            if(!executor.awaitTermination(3000, TimeUnit.MILLISECONDS)) executor.shutdownNow();
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            System.out.println("Il pool è stato chiuso forzatamente.");
+        }
+        System.out.println("Il pool è stato chiuso.");
+        System.out.println("Server closed.");
+
     }
 }
