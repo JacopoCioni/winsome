47beadcd7aafa42800371d358fb2b1f62aa35c9c
JacopoCioni <cionijacopo4@gmail.com>
2022-10-02 23:14:22 +0200

    Added variables

--- core/src/main/java/it/unipi/jcioni/winsome/core/service/WinsomeService.java
+++ core/src/main/java/it/unipi/jcioni/winsome/core/service/WinsomeService.java
@@ -11,6 +11,13 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 public interface WinsomeService extends Remote {
+    int SERVER_TCP_PORT = 8080;
+    int SERVER_RMI_PORT = 6969;
+    String RMI_SERVER_REGISTRY_NAME = "winsome-server";
+    int RMI_CALLBACK_CLIENT_PORT = 6970;
+    String RMI_CALLBACK_CLIENT_REGISTRY_NAME = "winsome-server-callback";
+    String SERVER_ADDRESS = "localhost";
+
     boolean register(String username, String password, String tags) throws RemoteException;
     boolean login(String username, String password) throws RemoteException;
     boolean logout(String username) throws RemoteException;
