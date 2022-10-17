7d176e2431c45b0257101a617f8124c48ef6c039
JacopoCioni <cionijacopo4@gmail.com>
2022-10-18 00:53:57 +0200

    Corretto metodo equals() per confrontare tags

--- core/src/main/java/it/unipi/jcioni/winsome/core/model/Tag.java
+++ core/src/main/java/it/unipi/jcioni/winsome/core/model/Tag.java
@@ -1,7 +1,9 @@
 package it.unipi.jcioni.winsome.core.model;
 
+import java.util.Objects;
+
 public class Tag {
-    private final String value;
+    private String value;
 
     public Tag(String value) {
         this.value = value.toLowerCase();
@@ -10,4 +12,13 @@ public class Tag {
     public String getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return value.equals(tag.value);
+    }
+
 }
