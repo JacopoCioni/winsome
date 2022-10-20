814f1fe967520e8296ceb5a37c50ab4a72236d0f
JacopoCioni <cionijacopo4@gmail.com>
2022-10-20 13:16:43 +0200

    Aggiunto var per ricordare il momento di pubblicazione del commento

--- core/src/main/java/it/unipi/jcioni/winsome/core/model/Comment.java
+++ core/src/main/java/it/unipi/jcioni/winsome/core/model/Comment.java
@@ -3,10 +3,12 @@ package it.unipi.jcioni.winsome.core.model;
 public class Comment {
     private String value;
     private String creator;
+    private long commentTime;
 
     public Comment(String creator, String value) {
         this.creator = creator;
         this.value = value;
+        this.commentTime = System.currentTimeMillis();
     }
 
     public String getValue() {
@@ -15,5 +17,9 @@ public class Comment {
 
     public String getCreator() {
         return creator;
+    }
+
+    public long getCommentTime() {
+        return commentTime;
     }
 }
