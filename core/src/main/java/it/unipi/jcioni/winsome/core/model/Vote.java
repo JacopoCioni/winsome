445bde14c3ce710da544d173e3bfb6253b2395dd
JacopoCioni <cionijacopo4@gmail.com>
2022-10-20 12:02:18 +0200

    Aggiunto tempo di pubblicazione voto

--- core/src/main/java/it/unipi/jcioni/winsome/core/model/Vote.java
+++ core/src/main/java/it/unipi/jcioni/winsome/core/model/Vote.java
@@ -5,12 +5,18 @@ public enum Vote {
     DOWN(-1);
 
     private final int value;
+    private final long voteTime;
 
     private Vote(int value) {
         this.value = value;
+        this.voteTime = System.currentTimeMillis();
     }
 
     public int getValue() {
         return value;
+    }
+
+    public long getVoteTime() {
+        return voteTime;
     }
 }
