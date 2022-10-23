8fd09976d44f57fa15c5ccda4581a4448eac3ebc
JacopoCioni <cionijacopo4@gmail.com>
2022-10-23 22:55:58 +0200

    Aggiunto oggetto Gson, aggiunti i metodi invia() e ricevi()

--- server/src/main/java/it/unipi/jcioni/winsome/server/WinsomeUtils.java
+++ server/src/main/java/it/unipi/jcioni/winsome/server/WinsomeUtils.java
@@ -1,5 +1,8 @@
 package it.unipi.jcioni.winsome.server;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
@@ -7,6 +10,13 @@ import java.net.URLConnection;
 public class WinsomeUtils {
     // TODO: Inserire in file di properties/ file yaml
     public static final String address = "https://www.random.org/decimal-fractions/?num=1&dec=20&col=1&format=plain&rnd=new";
+    public static Gson gson;
+    static {
+        gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .serializeNulls()
+                .create();
+    }
 
     // Generatore di numeri pseudo casuali utilizzando un servizio esterno
     // Per generare il numero faccio uso del servizio RANDOM.ORG
@@ -37,6 +47,28 @@ public class WinsomeUtils {
         // In caso di errore
         return 0;
     }
+
+    // Metodo per approssimare un numero al valore più vicino
+    public static double round(double in, int precision) {
+        double multi = Math.pow(10, precision);
+        return Math.round(in*multi)/multi;
+    }
+
+    // TODO: Sistemare i metodi di invio e ricezione in server/Handler* e client/Main*
+    //Metodi per invio e ricezione di streams di dati
+    // INVIO
+    private static void invia (PrintWriter output, String send) {
+        output.println(send);
+        output.flush();
+    }
+    // RICEVI
+    private static String ricevi (BufferedReader input) throws IOException {
+        String text = input.readLine();
+        if (text == null) throw new IOException();
+        text = text.replace('$', '\n');
+        return text;
+    }
+
 
     // Legge il contenuto di un file e lo converte in stringa
     public static String readFile(File file) throws IOException {
