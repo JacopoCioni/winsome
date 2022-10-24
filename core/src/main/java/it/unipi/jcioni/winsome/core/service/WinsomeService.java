783784d64cab7a4b827897af037026a4d1395835
JacopoCioni <cionijacopo4@gmail.com>
2022-10-24 23:48:07 +0200

    Fixato metodo startsFollowers(), adesso funziona con HashMap

--- core/src/main/java/it/unipi/jcioni/winsome/core/service/WinsomeService.java
+++ core/src/main/java/it/unipi/jcioni/winsome/core/service/WinsomeService.java
@@ -1,15 +1,9 @@
 package it.unipi.jcioni.winsome.core.service;
 
-import it.unipi.jcioni.winsome.core.exception.InvalidOperationException;
-import it.unipi.jcioni.winsome.core.exception.PostNotFoundException;
-import it.unipi.jcioni.winsome.core.exception.SameUserException;
-import it.unipi.jcioni.winsome.core.exception.UserNotFoundException;
-import it.unipi.jcioni.winsome.core.model.*;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+
 
 public interface WinsomeService extends Remote {
     int SERVER_TCP_PORT = 8080;
@@ -22,37 +16,5 @@ public interface WinsomeService extends Remote {
     String MULTICAST_ADDRESS = "224.0.0.1";
 
     boolean register(String username, String password, String tags) throws RemoteException;
-    ArrayList<String> startFollowers(String username, String password) throws RemoteException;
-
-    /*
-    boolean login(String username, String password) throws RemoteException;
-    boolean logout(String username) throws RemoteException;
-    List<User> listUsers(User user) throws RemoteException;
-    List<User> listFollowers(User user) throws RemoteException;
-    List<User> listFollowing(User following) throws RemoteException;
-    void followUser(String username, String following)
-            throws RemoteException, SameUserException, UserNotFoundException, InvalidOperationException;
-    void unfollowUser(String username, String followed)
-            throws RemoteException, SameUserException, UserNotFoundException, InvalidOperationException;
-    List<Post> viewBlog(User user) throws RemoteException;
-    void createPost(String title, String text, User u) throws RemoteException;
-    List<Post> showFeed(User user) throws RemoteException;
-
-    Post showPost(String idPost)
-            throws RemoteException, PostNotFoundException;
-
-    void deletePost(User user, String idPost)
-            throws RemoteException, InvalidOperationException, PostNotFoundException;
-
-    Wallet getWallet(User user) throws RemoteException;
-
-    void rewinPost(User user, String idPost) throws RemoteException;
-
-    void ratePost(String idPost, Vote vote, User user)
-            throws RemoteException, UserNotFoundException, PostNotFoundException, InvalidOperationException;
-
-    void addComment(String idPost, Comment comment, User user)
-            throws RemoteException, UserNotFoundException, PostNotFoundException, InvalidOperationException;
-
-     */
+    HashMap<String, String> startFollowers(String username, String password) throws RemoteException;
 }
