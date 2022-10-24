28c7ea7fc4365436383399d88d5bb8a229d3f9ca
JacopoCioni <cionijacopo4@gmail.com>
2022-10-24 23:52:29 +0200

    Fixato metodo save(), adesso salva anche follows & followers

--- server/src/main/java/it/unipi/jcioni/winsome/server/WinsomeSave.java
+++ server/src/main/java/it/unipi/jcioni/winsome/server/WinsomeSave.java
@@ -6,6 +6,7 @@ import it.unipi.jcioni.winsome.core.model.WinsomeData;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class WinsomeSave implements Runnable{
@@ -32,24 +33,45 @@ public class WinsomeSave implements Runnable{
         while (true) {
             // Autosave ogni minuto
             Thread.sleep(60000L);
+
             // Lista di utenti
             ConcurrentLinkedDeque<User> users = winsomeData.getUsers();
             // Lista di TUTTI i post della piattaforma
             ConcurrentLinkedDeque<Post> allPosts = new ConcurrentLinkedDeque<>();
-            // Aggiungo tutti i post di tutti gli utenti a questa lista
+            // HashMap di TUTTI i follows della piattaforma
+            HashMap<String, String> allFollows = new HashMap<>();
+            // HashMap di TUTTI i followers della piattaforma
+            HashMap<String, String> allFollowers = new HashMap<>();
+
+            // Scorro tutti gli utenti della piattaforma
             for (User u: users) {
+                // Aggiungo tutti i post di tutti gli utenti a questa lista
                 if (u.getBlog() != null) {
                     allPosts.addAll(u.getBlog().getPosts());
                 }
+                if (u.getInteractions() != null) {
+                    // Aggiungo tutti i follows di tutti gli utenti della piattaforma
+                    allFollows.putAll(u.getInteractions().getFollows());
+                    // Aggiungo tutti i followers di tutti gli utenti della piattaforma
+                    allFollowers.putAll(u.getInteractions().getFollowers());
+                }
             }
-            //Scrivo gli utenti su file
+
+            // Scrivo gli utenti su file
             String jsonUsers = WinsomeUtils.gson.toJson(users);
             WinsomeUtils.writeFile(jsonUsers, "WinsomeServer"+ File.separator+"Users.json");
-            //Scrivo i post su file
+            // Scrivo i post su file
             String jsonPosts = WinsomeUtils.gson.toJson(allPosts);
             WinsomeUtils.writeFile(jsonPosts, "WinsomeServer"+ File.separator+"Posts.json");
+            // Scrivo i follows su file
+            String jsonFollows = WinsomeUtils.gson.toJson(allFollows);
+            WinsomeUtils.writeFile(jsonFollows,"WinsomeServer"+ File.separator+"Follows.json");
+            // Scrivo tutti i followers su file
+            String jsonFollowers = WinsomeUtils.gson.toJson(allFollowers);
+            WinsomeUtils.writeFile(jsonFollowers,"WinsomeServer"+ File.separator+"Followers.json");
 
             System.out.println("[SERV] - Backup eseguito.");
+
         }
     }
 }
