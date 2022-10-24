a5a590d5dd3342e667b15878baa06192209c026e
JacopoCioni <cionijacopo4@gmail.com>
2022-10-24 23:46:53 +0200

    Fixati metodi per le HashMap

--- core/src/main/java/it/unipi/jcioni/winsome/core/service/WinsomeNotifyEvent.java
+++ core/src/main/java/it/unipi/jcioni/winsome/core/service/WinsomeNotifyEvent.java
@@ -4,6 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface WinsomeNotifyEvent extends Remote {
-    void addNotifyEvent (String value) throws RemoteException;
+    void addNotifyEvent (String mainUser, String value) throws RemoteException;
     void removeNotifyEvent (String value) throws RemoteException;
 }
